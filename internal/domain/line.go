61a6f8c0f071cbf75315e8466ac6bb41704a173d
cristianSanta <cristiansanta5@gmail.com>
2024-11-25 09:17:53 -0500

    Consultas lineas de atencion APE y RAV

---
+++ internal/domain/line.go
@@ -0,0 +1,10 @@
+package domain
+
+	type LineaAtencion struct {
+		Departamento     string
+		Ciudad           string
+		Contacto         string
+		Direccion        string
+		HorarioAtencion  string
+		Telefono         string
+}
